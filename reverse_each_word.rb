56a2ff149149a4aa09d36aadaf4c588ef9abb8fc
Scott Silverman <scottms724@gmail.com>
2018-04-30 15:44:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   reversedWords = []
   string = string.split(" ")
-  string.each do |word|
+  string.collect do |word|
     word = word.reverse!
     reversedWords.push(word)
   end
